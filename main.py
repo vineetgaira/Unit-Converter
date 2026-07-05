5b0f064913d92d4e6ddf42c049f1ade222d42863
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-05 09:01:58 +0530

    Create Initial Project Skeleton

---
+++ main.py
@@ -0,0 +1,38 @@
+import colorama 
+from colorama import Fore, Back, Style
+
+def display_menu():
+    print(Fore.LIGHTGREEN_EX+"...Welcome to unit converter...\n" \
+    "Choose category:\n" \
+    "1 : Length\n" \
+    "2 : Weight\n" \
+    "3 : Temperature\n" \
+    "4 : Time\n" \
+    "6 : Speed\n" \
+    "8 : Area\n" \
+    "9 : Volume"+Fore.RESET)
+
+def length():
+    pass
+def weight():
+    pass
+def temperature():
+    pass
+def time():
+    pass
+def speed():
+    pass
+def area():
+    pass
+def volume():
+    pass
+def choose_from_unit():
+    pass
+def choose_to_unit():
+    pass
+def convert():
+    pass
+def show_result():
+    pass
+
+display_menu()
