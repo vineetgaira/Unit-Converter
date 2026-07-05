f605227672a9041635360f9cad25091748e047dc
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-05 09:08:12 +0530

    Create user_choice logic

--- main.py
+++ main.py
@@ -10,7 +10,22 @@ def display_menu():
     "4 : Time\n" \
     "6 : Speed\n" \
     "8 : Area\n" \
-    "9 : Volume"+Fore.RESET)
+    "9 : Volume\n"
+    "10 : Exit"+Fore.RESET)
+
+def user_choice():
+    valid_choices={1,2,3,4,5,6,7,8,9}
+    while True:
+        try:
+            user_choice=int(input("Choose your category :"))
+            if user_choice in valid_choices:
+                return user_choice
+            else:
+                print("Please enter a valid category from the menu.")
+        except ValueError:
+            print("Please enter a valid integer option.")
+            
+        
 
 def length():
     pass
@@ -35,4 +50,4 @@ def convert():
 def show_result():
     pass
 
-display_menu()
+user_choice()
